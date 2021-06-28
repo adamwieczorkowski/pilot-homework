190f9e198202d4cf7cdea9fccc4633a64b303ca7
Adam Wieczorkowski <adam.wieczorkowski@naturaily.com>
2021-06-28 15:52:04 +0200

    contractor/manager: added zookeeper, kafka and karafka

---
+++ manager/karafka.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Ruby on Rails setup
+ENV["RAILS_ENV"] ||= "development"
+ENV["KARAFKA_ENV"] = ENV["RAILS_ENV"]
+require ::File.expand_path("../config/environment", __FILE__)
+Rails.application.eager_load!
+
+class KarafkaApp < Karafka::App
+  setup do |config|
+    config.kafka.seed_brokers = %w(kafka://kafka:9092)
+    config.kafka.start_from_beginning = false
+    config.client_id = "main_app"
+    config.backend = :inline
+    config.batch_fetching = true
+    config.batch_consuming = false
+    config.logger = Logger.new(STDOUT, level: :info)
+  end
+
+  consumer_groups.draw do
+    # topic :payments do
+    #   consumer PaymentsConsumer
+    # end
+  end
+end
+
+KarafkaApp.boot!
